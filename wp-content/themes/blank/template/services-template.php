d3c60d67a1f9b3b79539d578687e4e13a9828a98
unknown <you@example.com>
2021-05-12 00:19:18 +0800

    First Back Up

---
+++ wp-content/themes/blank/template/services-template.php
@@ -0,0 +1,27 @@
+<?php 
+/* Template Name: SERVICES */
+get_header();
+ ?>
+<div >
+	<a class="wedding"href="">Wedding Package</a>
+	<p class="wp">-Pre Nup Photos and Video <br> -Same-Day-Edit Video <br>-Photobooth <br>-Full Wedding Coverage (8-10HRS) <br>-FlashDrive & Copyright of Photograph with album(800+PHOTOS) <br>-Full Video Coverage <br>-1big Frame</p>
+
+	<a class="bday"href="">Birthday Package</a>
+	<p class="hbd">-Pre Photos and Video <br> -Same-Day-Edit Video <br>-Photobooth <br>-Full Wedding Coverage (3-6HRS) <br>-FlashDrive & Copyright of Photograph with album(800+PHOTOS) <br>-Full Video Coverage <br>-1big Frame</p>
+
+	<a class="bday1"href="">1st Birthday Package</a>
+	<p class="hbd1">-Pre Photos and Video <br> -Same-Day-Edit Video <br>-Photobooth <br>-Full Wedding Coverage (3-6HRS) <br>-FlashDrive & Copyright of Photograph with album(800+PHOTOS) <br>-Full Video Coverage <br>-1big Frame</p>
+
+	<a class="pb"href="">PhotoBooth Package</a>
+	<p class="pbp">-FlashDrive & Copyright of Photograph with album(800+PHOTOS)</p>
+
+
+	<img class="img1" src="http://localhost/kc/wp-content/uploads/2021/05/180510295_764863017541156_1019511004480587975_n.jpg"><br>
+	<img class="img2"src="http://localhost/kc/wp-content/uploads/2021/05/181386553_923198521857984_4226267777397977797_n.jpg"><br>
+	<img class="img3"src="http://localhost/kc/wp-content/uploads/2021/05/181746334_537356964303690_5516878281292477074_n.jpg"><br>
+	<img class="img4"src="http://localhost/kc/wp-content/uploads/2021/05/182472723_2913783708879347_7555347336167225983_n.jpg">
+</div>
+
+<div>
+	
+</div>
